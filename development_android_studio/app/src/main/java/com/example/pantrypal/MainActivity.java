486b5d3d1fc995a0f41df87e6de22daad2aeab4d
Ari Braverman <brave057@umn.edu>
2023-03-30 10:55:00 -0500

    hopefully this works?

--- development_android_studio/app/src/main/java/com/example/pantrypal/MainActivity.java
+++ development_android_studio/app/src/main/java/com/example/pantrypal/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
--- development_android_studio/app/src/main/java/com/example/pantrypal/Notifications.java
+++ development_android_studio/app/src/main/java/com/example/pantrypal/Notifications.java
@@ -2,7 +2,10 @@ package com.example.pantrypal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Notifications extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Notifications extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        ImageButton notificationsBackButton = (ImageButton) findViewById(R.id.notification_back_button);
+        notificationsBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Notifications.this, Settings.class);
+                startActivity(intent);
+            }
+        });
     }
 }
