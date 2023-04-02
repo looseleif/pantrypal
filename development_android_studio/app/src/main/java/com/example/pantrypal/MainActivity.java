cd2c6d5d6857cea6e26364baa80bdfdacb84e3be
Emir Sahbegovic <esahbegovic@gmail.com>
2023-04-02 06:33:58 -0500

    updated layout

--- development_android_studio/app/src/main/java/com/example/pantrypal/MainActivity.java
+++ development_android_studio/app/src/main/java/com/example/pantrypal/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.pantrypal;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,12 +13,33 @@ import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO implement lists for expiring soon and receipts
+        ArrayList<Item> recentReceipts = new ArrayList<Item>();
+        ArrayList<Item> expiringSoon = new ArrayList<Item>();
+
+        //RecyclerView
+        RecyclerView recentReceiptView = (RecyclerView) findViewById(R.id.recentReceiptList);
+        recentReceiptView.setHasFixedSize(true);
+        recentReceiptView.setLayoutManager(new LinearLayoutManager(this));
+        //add list of objects to adapter
+        ItemAdapter recentRAdapter = new ItemAdapter(this, recentReceipts);
+        recentReceiptView.setAdapter(recentRAdapter);
+
+        RecyclerView expiringView = (RecyclerView) findViewById(R.id.expiringList);
+        expiringView.setHasFixedSize(true);
+        expiringView.setLayoutManager(new LinearLayoutManager(this));
+
+        ItemAdapter expiringAdapter = new ItemAdapter(this, expiringSoon);
+        expiringView.setAdapter(expiringAdapter);
 
         ImageButton settingsButton = (ImageButton) findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(new View.OnClickListener() {
