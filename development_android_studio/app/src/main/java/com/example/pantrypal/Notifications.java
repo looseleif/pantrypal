7e8ff2f59c0df7f4b093f7bc5ffd27ccfd1b3d71
Chase <chase.anderson.000@gmail.com>
2023-04-05 20:45:17 -0500

    hide action

--- development_android_studio/app/src/main/java/com/example/pantrypal/Notifications.java
+++ development_android_studio/app/src/main/java/com/example/pantrypal/Notifications.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 public class Notifications extends AppCompatActivity {
@@ -13,6 +15,8 @@ public class Notifications extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        getSupportActionBar().hide();
 
         ImageButton notificationsBackButton = (ImageButton) findViewById(R.id.notification_back_button);
         notificationsBackButton.setOnClickListener(new View.OnClickListener() {
