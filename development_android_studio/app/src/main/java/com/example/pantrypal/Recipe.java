4b43bf5bcfd0537ae1e89e22d1c79c76b47f1c7a
Chase <chase.anderson.000@gmail.com>
2023-04-06 06:21:43 -0500

    good for now

--- development_android_studio/app/src/main/java/com/example/pantrypal/Recipe.java
+++ development_android_studio/app/src/main/java/com/example/pantrypal/Recipe.java
@@ -61,13 +61,16 @@ public class Recipe extends AppCompatActivity {
 
         fullInventory = new ArrayList<Item>();
 
-        Item milk = new Item(0, "Milk", "10/11/2023", "Diary", 1, "Fridge");
-        Item ice_cream = new Item(1, "Chicken", "10/12/2023", "Diary", 1, "Freezer");
-        Item apples = new Item(2, "Apples", "10/11/2023", "Fruit", 3, "Cabinet");
+        Item milk = new Item(0, "Milk", "4/06/2023", "Diary", 1, "Fridge");
+        Item chicken = new Item(1, "Chicken", "4/07/2023", "Diary", 1, "Freezer");
+        Item ice_cream = new Item(2, "Ice Cream", "4/12/2023", "Diary", 1, "Freezer");
+        Item apples = new Item(3, "Apples", "10/11/2023", "Fruit", 3, "Cabinet");
         inventory.addFridgeItem(milk);
+        inventory.addFridgeItem(chicken);
         inventory.addFreezerItem(ice_cream);
         inventory.addCabinetItem(apples);
         fullInventory.add(milk);
+        fullInventory.add(chicken);
         fullInventory.add(ice_cream);
         fullInventory.add(apples);
         //create recycler view object and set layout info
