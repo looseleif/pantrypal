32ec3320b8cdc759af23eb2e0ce4e4889d55f6d7
Emir Sahbegovic <esahbegovic@gmail.com>
2023-04-05 22:50:44 -0500

    settings clear button

--- development_android_studio/app/src/main/java/com/example/pantrypal/Settings.java
+++ development_android_studio/app/src/main/java/com/example/pantrypal/Settings.java
@@ -3,6 +3,7 @@ package com.example.pantrypal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +40,17 @@ public class Settings extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Settings.this, AboutActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button clearButton = (Button) findViewById(R.id.clear_inventory);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
             }
         });
     }
